1d68970017b98a231535084b41cff57ca83711ad
windyka <officialwindyka@gmail.com>
2018-12-19 04:55:03 +0700

    create new list

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -5,14 +5,32 @@ class Todo extends Component {
         database: [' satu', ' dua', ' tiga', ' empat'],
         input: ''
     }
+    // menetapkan nilai baru sebagai input baru saat target {input} mengalami perubahan
+    handleChange = (e) => {
+        this.setState({
+            input: e.target.value
+        })
+        console.log(this.state.input)
+    }
+
+    addData = () => {
+        const { database } = this.state
+        const { input } = this.state
+        database.push(input)
+        console.log(database)
+        this.setState({
+            input: ''
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>Todolist</h1>
-                <input type="text" />
-                <button type="submit">Submit</button>
+                <input type="text" onChange={this.handleChange} value={this.state.input} /> <br />
+                <button onClick={() => this.addData()} type="submit">Submit</button>
                 <ul>
-                    {this.state.database.map(paramet => { return <li>{paramet}</li>; }
+                    {this.state.database.map(datab => { return <li>{datab}</li>; }
 
                     )}
                 </ul>
